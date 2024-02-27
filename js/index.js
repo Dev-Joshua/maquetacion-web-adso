dd3083249b9a8d8078ab29aa8b6b513c432fe42f
Joshua Daniel <joshua_dup@hotmail.com>
2024-02-27 13:21:35 -0500

    update estilos

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-const containerImages = document.querySelector(".container-images");
-const puntos = document.querySelectorAll(".punto");
+// const containerImages = document.querySelector(".container-images");
+// const puntos = document.querySelectorAll(".punto");
 
 //Recorrer los 3 puntos(li) y asignar un click a cada punto(i)
-puntos.forEach((punto, i) => {
-  puntos[i].addEventListener("click", () => {
-    let position = i;
-    let operation = position * -50;
+// puntos.forEach((punto, i) => {
+//   puntos[i].addEventListener("click", () => {
+//     let position = i;
+//     let operation = position * -50;
 
-    containerImages.style.transform = `translateX(${operation}%)`;
+//     containerImages.style.transform = `translateX(${operation}%)`;
 
-    puntos.forEach((punto, i) => {
-      puntos[i].classList.remove("active");
-    });
+//     puntos.forEach((punto, i) => {
+//       puntos[i].classList.remove("active");
+//     });
 
-    puntos[i].classList.add("active");
-  });
-});
+//     puntos[i].classList.add("active");
+//   });
+// });
 
 //posicioon en 0 transformX = 0
 //posicion en 1 transformX = -50%
