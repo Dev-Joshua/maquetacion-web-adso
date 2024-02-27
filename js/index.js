d290600a5a6569439260b032d4256ae407482802
Joshua Daniel <joshua_dup@hotmail.com>
2024-02-27 13:05:01 -0500

    termino maquetado del home con el footer

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const containerImages = document.querySelector(".container-images");
+const puntos = document.querySelectorAll(".punto");
+
+//Recorrer los 3 puntos(li) y asignar un click a cada punto(i)
+puntos.forEach((punto, i) => {
+  puntos[i].addEventListener("click", () => {
+    let position = i;
+    let operation = position * -50;
+
+    containerImages.style.transform = `translateX(${operation}%)`;
+
+    puntos.forEach((punto, i) => {
+      puntos[i].classList.remove("active");
+    });
+
+    puntos[i].classList.add("active");
+  });
+});
+
+//posicioon en 0 transformX = 0
+//posicion en 1 transformX = -50%
+//operacion = position * -50
